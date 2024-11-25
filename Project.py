c7ab980db699d68d2ac521bbbf7c03f55e1b8b11
Abdykadyr Turduev <abdykadyr.turduev@alatoo.edu.kg>
2024-11-25 15:02:29 +0600

    Ninth commit.py

--- Project.py
+++ Project.py
@@ -3,7 +3,7 @@ def find_cargo():
     weight = [0, 0, 0] 
     total_weight = 0
     total_cargo = 0
-    while total_weight != 713;
+    while total_weight != 713:
           print("Enter 3 kilometrs and corresponding weights for the 3 cargo boxes:")
           for i in range(3):
               positions[i] = int(input(f"Enter the distance (km) for box {i+1}: "))
