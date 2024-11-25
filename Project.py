a2b4149cd94cee4e1792962aabbd9e2270d80836
Abdykadyr Turduev <abdykadyr.turduev@alatoo.edu.kg>
2024-11-25 14:47:15 +0600

    second commit.py

--- Project.py
+++ Project.py
@@ -3,6 +3,9 @@ def find_cargo():
     weight = [0, 0, 0] 
     total_weight = 0
     total_cargo = 0
+    while total_weight != 713;
+          print("Enter 3 kilometrs and corresponding weights for the 3 cargo boxes:")
+    
     
 
 
