a6b6644eb4d1951eeda85cbd463f480ab726c5fd
Abdykadyr Turduev <abdykadyr.turduev@alatoo.edu.kg>
2024-11-25 15:08:26 +0600

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,13 +1,13 @@
 def find_cargo():
     positions = [0, 0, 0]
-    weights = [0, 0, 0] 
+    weight = [0, 0, 0] 
     total_weight = 0
     total_cargo = 0
     while total_weight != 713:
           print("Enter 3 kilometrs and corresponding weights for the 3 cargo boxes:")
           for i in range(3):
               positions[i] = int(input(f"Enter the distance (km) for box {i+1}: "))
-              weights[i] = int(input(f"Enter the weight for box {i+1}: "))
+              weight[i] = int(input(f"Enter the weight for box {i+1}: "))
           print("Enter 3 kilometrs to check where the cargo: ")
           km_inputs = [int(input(f"Enter kilometer mark {i+1}: ")) for i in range(3)]
           total_weight = 0
@@ -15,7 +15,7 @@ def find_cargo():
           for km in km_inputs:
               if km in positions:
                   index = positions.index(km)
-                  total_weight += weights[index]
+                  total_weight += weight[index]
                   total_cargo += 1
           if total_cargo == 3 and total_weight == 713:
               print(f"Cargo found!")
